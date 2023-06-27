b3302d145a2ef8569dd13d83784dbd8a8f40e979
Ben Weber <benweberswe@gmail.com>
2023-06-26 22:07:53 -0700

    working through detection with crash issue on success

--- cameraDetection.py
+++ cameraDetection.py
@@ -7,10 +7,17 @@ cap.set(3, 640)
 cap.set(4, 420)
 
 # Color detection parameters
-lower_black = np.array([0, 0, 0])  # Lower range of black color (in HSV)
-upper_black = np.array([180, 255, 30])  # Upper range of black color (in HSV)
-lower_white = np.array([0, 0, 200])  # Lower range of white color (in HSV)
-upper_white = np.array([180, 30, 255])  # Upper range of white color (in HSV)
+""" lower_black = np.array([0], dtype=np.uint8)  # Lower grayscale value for black
+upper_black = np.array([60], dtype=np.uint8)  # Upper grayscale value for black (dark gray)
+
+lower_white = np.array([160], dtype=np.uint8)  # Lower grayscale value for white (including gray)
+upper_white = np.array([255], dtype=np.uint8)  # Upper grayscale value for white """
+
+lower_black = np.array([0, 0, 0], dtype=np.uint8)  # Lower BGR value for black
+upper_black = np.array([60, 60, 60], dtype=np.uint8)  # Upper BGR value for black (dark gray)
+
+lower_white = np.array([160, 160, 160], dtype=np.uint8)  # Lower BGR value for white (including gray)
+upper_white = np.array([255, 255, 255], dtype=np.uint8)  # Upper BGR value for white
 
 # import cascade file for facial recognition
 # https://github.com/opencv/opencv/tree/master/data/haarcascades
@@ -19,12 +26,14 @@ catFaceCascadeExtended = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcasc
 
 while True:
     success, img = cap.read()
-    imgHSV  = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+    # imgGrey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-    facesExt = catFaceCascadeExtended.detectMultiScale(imgHSV, scaleFactor=1.05, minNeighbors=6, minSize=(30,30))
-    
-    for (x, y, w, h) in facesExt:
-        roi = imgHSV[y:y + h, x:x + w]  # Region of interest
+    facesExt = catFaceCascade.detectMultiScale(img, scaleFactor=1.5, minNeighbors=2)
+    facesExt2 = catFaceCascadeExtended.detectMultiScale(img, scaleFactor=1.5, minNeighbors=2)
+    combined_faces = np.concatenate((facesExt, facesExt2), axis=0)
+
+    for (x, y, w, h) in combined_faces:
+        roi = img[y:y + h, x:x + w]  # Region of interest
 
          # Detect black color
         mask_black = cv2.inRange(roi, lower_black, upper_black)
@@ -32,19 +41,19 @@ while True:
 
         # Check if the subject is black
         if len(contours_black) > 0:
-            cv2.drawContours(imgHSV, contours_black, -1, (0, 0, 0), 2)
-            cv2.putText(imgHSV, 'Tater', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
+            cv2.drawContours(img, contours_black, -1, (0, 0, 0), 2)
+            cv2.putText(img, 'Tater', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 0, 0), 2)
         else:
             # Detect white color
             mask_white = cv2.inRange(roi, lower_white, upper_white)
             contours_white, _ = cv2.findContours(mask_white, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-            cv2.drawContours(imgHSV, contours_white, -1, (255, 255, 255), 2)
-            cv2.putText(imgHSV, 'Pee', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
+            cv2.drawContours(img, contours_white, -1, (255, 255, 255), 2)
+            cv2.putText(img, 'Pee', (x, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 255, 255), 2)
 
-        cv2.rectangle(imgHSV, (x, y), (x + w, y + h), (0, 255, 0), 3)
-        cv2.putText(imgHSV, 'Cat', (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+        cv2.rectangle(img, (x, y), (x + w, y + h), (0, 255, 0), 3)
+        cv2.putText(img, 'Cat', (x, y - 30), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
 
-    cv2.imshow('cat_detect', imgHSV)
+    cv2.imshow('cat_detect', img)
     if cv2.waitKey(10) & 0xFF == ord('q'):
         break
 
